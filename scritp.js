d89358bf122cae4b07470d8c226808b7fd6f125c
subJobANewLice <nadonadoyz@gmail.com>
2024-01-05 15:47:48 +0900

    add font

--- scritp.js
+++ scritp.js
@@ -1,5 +1,7 @@
 const disableFont = document.querySelector(".disable-font");
+const addFont = document.querySelector(".add-font");
 
 disableFont.addEventListener("click", () => {
     disableFont.remove();
+    addFont.insertAdjacentHTML("afterbegin","<h1>ここでは私が調べてきた情報をまとめております<h1>");
 });
