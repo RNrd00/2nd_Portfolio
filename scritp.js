79d85c9f237493766fe14ea8d5817ec401fad3aa
subJobANewLice <nadonadoyz@gmail.com>
2024-01-07 15:15:19 +0900

    add content number

--- scritp.js
+++ scritp.js
@@ -1,7 +1,12 @@
 const disableFont = document.querySelector(".disable-font");
 const addFont = document.querySelector(".add-font");
+const contentNumber = document.querySelectorAll(".content-number > h1");
 
 disableFont.addEventListener("click", () => {
     disableFont.remove();
     addFont.insertAdjacentHTML("afterbegin","<h1 class='disable-font'>ここでは私が調べてきた情報をまとめております<h1>");
 });
+
+for(let i = 0; i < contentNumber.length; i++) {
+    contentNumber[i].insertAdjacentHTML("afterbegin",i + ".");
+}
