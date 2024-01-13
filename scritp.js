186e3371a6f924731af25969e93fd2ac81e40373
subJobANewLice <nadonadoyz@gmail.com>
2024-01-13 15:35:10 +0900

    observer test

--- scritp.js
+++ scritp.js
@@ -1,6 +1,19 @@
 const disableFont = document.querySelector(".disable-font");
 const addFont = document.querySelector(".add-font");
 const contentNumber = document.querySelectorAll(".content-number > h1");
+const observerTest = document.querySelector(".observer-test");
+
+const callback = function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("inView");
+        } else {
+            entry.target.classList.remove("inView");
+        }
+    });
+}
+
+const ot = new IntersectionObserver(callback);
 
 disableFont.addEventListener("click", () => {
     disableFont.remove();
@@ -10,3 +23,7 @@ disableFont.addEventListener("click", () => {
 for(let i = 0; i < contentNumber.length; i++) {
     contentNumber[i].insertAdjacentHTML("afterbegin",i + ".");
 }
+
+ot.observe(observerTest);
+
+//https://qiita.com/greencha/items/ee49e2acd885eddc5570 ←ここを参照中
